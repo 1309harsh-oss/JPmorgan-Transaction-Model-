382a315328944fc6c523b094de3386e40fe996d7
1309harsh-oss <1309harsh@gmail.com>
2026-06-09 01:49:01 +0530

    Transaction Model

---
+++ src/main/java/com/example/jpmorgan1/JPmorgan1Application.java
@@ -0,0 +1,13 @@
+package com.example.jpmorgan1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JPmorgan1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JPmorgan1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jpmorgan1/JPmorgan1ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.jpmorgan1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskThreeTests {
+
+    @Test
+    void testExample() {
+        System.out.println();
+    }
+
+}
